b3cdf6a2b47970cfdecd662c6d7bc424c5f2e8b0
GuiVMolina <guilhermevieiramolina@gmail.com>
2023-03-15 01:10:49 -0300

    Aula sobre JavaScript com varíáveis e funções

---
+++ Exercícios/Atividade01.js
@@ -0,0 +1,25 @@
+var adição
+var subtracao
+var multiplicação
+var divisão
+
+function funcaoAdição (a, b) {
+    return a + b
+}
+
+function funcaoSubtracao(a, b) {
+    return a - b
+}
+
+function funcaoMultiplicacao(a, b) {
+    return a * b
+}
+
+function funcaoDivisao(a, b) {
+    return a / b
+}
+
+console.log(funcaoAdição(10, 5))
+console.log(funcaoSubtracao(55, 40))
+console.log(funcaoMultiplicacao(5, 3))
+console.log(funcaoDivisao(90, 6))
